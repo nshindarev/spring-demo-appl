8df06e20d3d7ac96faefcbd1e7d95591e4e4c90a
Shindarev Nikita <nshindarev@gmail.com>
2022-11-05 13:39:43 +0300

    Spring initializr project created. dependencies included:
    - spring web
    - postgresql driver
    - spring data jpa

---
+++ src/test/java/com/example/nshindarev/demo/SpringTestApplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nshindarev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
